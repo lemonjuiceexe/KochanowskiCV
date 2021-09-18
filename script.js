cf5fe0c9fe848684a3be7f7984cce879e2af2ffa
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2021-09-18 21:11:57 +0200

    Add content, scrolling effect

--- script.js
+++ script.js
@@ -4,55 +4,79 @@ const s = document.getElementById("two");
 const sc = document.getElementById("in2");
 const t = document.getElementById("three");
 const tc = document.getElementById("in3");
+const tctt = document.getElementById("tt3");
+const fo = document.getElementById("four");
+const foc = document.getElementById("in4");
+const fv = document.getElementById("five");
+const fvc = document.getElementById("in5");
+
+const defaultColor = "yellowgreen";
+
+const smallFont = "80%";
+const smallerBig = "90%";
+const bigFont = "105%";
 
 document.addEventListener('scroll', () =>
 {
-    let fOffset = Math.abs(Offset(f));
-    let sOffset = Math.abs(Offset(s));
-    let tOffset = Math.abs(Offset(t));
-    //console.log(fOffset);
-    //console.log("webscroll " + window.scrollY)
-    if(fOffset < sOffset && fOffset < tOffset)
+    const fOffset = Math.abs(Offset(f));
+    const sOffset = Math.abs(Offset(s));
+    const tOffset = Math.abs(Offset(t));
+    const foOffset = Math.abs(Offset(fo));
+    const fvOffset = Math.abs(Offset(fv));
+
+    if(fOffset < sOffset && fOffset < tOffset && fOffset < foOffset && fOffset < fvOffset)
     {
-        f.style.backgroundColor = "yellow";
-        fc.style.fontSize = "120%";
+        //f.style.backgroundColor = "yellow";
+        fc.style.fontSize = bigFont;
     }
     else
     {
-        f.style.backgroundColor = "limegreen";
-        fc.style.fontSize = "80%";
+        //f.style.backgroundColor = "defaultColor";
+        fc.style.fontSize = smallFont;
     }
-    if(sOffset < fOffset && sOffset < tOffset)
+    if(sOffset < fOffset && sOffset < tOffset && sOffset < foOffset && sOffset < fvOffset)
     {
-        s.style.backgroundColor = "yellow";
-        sc.style.fontSize = "120%";
+        //s.style.backgroundColor = "yellow";
+        sc.style.fontSize = bigFont;
     }
     else
     {
-        s.style.backgroundColor = "crimson";
-        sc.style.fontSize = "80%";
+        //s.style.backgroundColor = "defaultColor";
+        sc.style.fontSize = smallFont;
     }
-    if(tOffset < sOffset && tOffset < fOffset)
+    if(tOffset < sOffset && tOffset < fOffset && tOffset < foOffset && tOffset < fvOffset)
     {
-        t.style.backgroundColor = "yellow";
-        tc.style.fontSize = "120%";
+        //t.style.backgroundColor = "yellow";
+        tc.style.fontSize = smallerBig;
+        tctt.style.fontSize = "130%";
     }
     else
     {
-        t.style.backgroundColor = "dodgerblue";
-        tc.style.fontSize = "80%";
+        //t.style.backgroundColor = "defaultColor";
+        tc.style.fontSize = smallFont;
+        tctt.style.fontSize = "";
     }
+    if(foOffset < sOffset && foOffset < tOffset && foOffset < fOffset && foOffset < fvOffset)
+    {
+        foc.style.fontSize = bigFont;
+    }
+    else
+    {
+        foc.style.fontSize = smallFont;
+    }
+    if(fvOffset < sOffset && fvOffset < tOffset && fvOffset < fOffset && fvOffset < foOffset)
+    {
+        fvc.style.fontSize = bigFont;
+    }
+    else
+    {
+        fvc.style.fontSize = smallFont;
+    }
+
 });
 
 function Offset(e)
 {
-    let r = e.getBoundingClientRect();
-    /*let top = r.top;
-    let s = window.scrollY;
-    return top + s;*/
-    // let b = getComputedStyle(e);
-    // let marginT = parseInt(b.marginTop);
-    // let marginB = parseInt(b.marginBottom);
-    console.log(r.height);
+    const r = e.getBoundingClientRect();
     return r.top - ((1/32) * r.height);
 }
